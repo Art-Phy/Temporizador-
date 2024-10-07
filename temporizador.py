598db22a7f1b1562906f0138c003b5f9819219a5
ArturoGit <arturosanma@gmail.com>
2024-10-07 13:23:10 +0200

    timer -= 1 added

--- temporizador.py
+++ temporizador.py
@@ -2,14 +2,15 @@
 
 import time
 
-def temporizador(segundos): #minutos, horas):
+def temporizador(segundos):
     while segundos:
-        min = segundos // 60
+        min =segundos // 60
         seg = segundos % 60
         hor = segundos // 3600
-        tiempo_restante = f"{min:02d}:{seg:02d}:{hor:02d}"
+        tiempo_restante = f"{hor:02d}:{min:02d}:{seg:02d}" # ese formato indica el número de dígitos
         print(tiempo_restante, end="\r")
         time.sleep(1)
+        segundos-= 1
     print("Tiempo finalizado")
 
 tiempo_ingresado = int(input("Ingresa el tiempo: "))
