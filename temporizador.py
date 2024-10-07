7cf039f744e71d0f2f133a4fbaf455ea4c5cb3b7
ArturoGit <arturosanma@gmail.com>
2024-10-07 16:05:30 +0200

    timer without interface

--- temporizador.py
+++ temporizador.py
@@ -3,17 +3,35 @@
 import time
 
 def temporizador(segundos):
-    while segundos:
-        min =segundos // 60
-        seg = segundos % 60
+    while segundos > 0:
         hor = segundos // 3600
-        tiempo_restante = f"{hor:02d}:{min:02d}:{seg:02d}" # ese formato indica el número de dígitos
+        min = (segundos % 3600) //60
+        seg = segundos % 60
+        # aquí se indica el formato del temporizador. 2 dígitos, horas, minutos, segundos
+        tiempo_restante = f"{hor:02d}:{min:02d}:{seg:02d}"
         print(tiempo_restante, end="\r")
         time.sleep(1)
         segundos-= 1
     print("Tiempo finalizado")
 
-tiempo_ingresado = int(input("Ingresa el tiempo: "))
+# func para aceptar horas, min y seg en diferentes formatos
+def convertir_a_segundos(tiempo):
+    horas, minutos, segundos = 0, 0, 0
+    if 'h' in tiempo:
+        horas = int(tiempo.split('h')[0])
+        tiempo = tiempo.split('h')[1]
+    if 'm' in tiempo:
+        minutos = int(tiempo.split('m')[0])
+        tiempo = tiempo.split('m')[1]
+    if 's' in tiempo:
+        segundos = int(tiempo.split('s')[0])
+        tiempo = tiempo.split('s')[1]
 
-temporizador(tiempo_ingresado)
+    total_segundos = horas *3600 + minutos *60 + segundos
+    return total_segundos
+
+tiempo_ingresado = (input("Ingresa el tiempo (indica si son horas, minutos, segundos): "))
+segundos_totales = convertir_a_segundos(tiempo_ingresado)
+
+temporizador(segundos_totales)
         
